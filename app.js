ec34e8dd47616d68d4e47e4c34229f77bad37ed6
tpaivaa <tpaivaa@gmail.com>
2017-12-10 19:22:07 +0200

    api keys from app config

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('./routes/sinch');
- 
+const siptestkey = process.env.siptestkey;
+const siptestsecret = process.env.siptestsecret;
+
 //set up an app
 var app = express();
 //configure on what port express will create your app
